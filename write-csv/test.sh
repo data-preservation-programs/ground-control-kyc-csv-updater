6fc3bed5269d14f92986b98074bbd5df919ac411
Jim Pick <jim@jimpick.com>
2022-08-03 12:59:33 -0700

    Add more logging to debug path issue

--- write-csv/test.sh
+++ write-csv/test.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-node index.js testdata/test-results.json sp-list.csv organizations.csv processed.csv
+#node index.js testdata/test-results.json sp-list.csv organizations.csv processed.csv
+node dist/index.js testdata/test-results.json sp-list.csv organizations.csv processed.csv
 
