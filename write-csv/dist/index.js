415dd594434a61f334647ff6aa15eeb37f55dec5
Jim Pick <jim@jimpick.com>
2022-08-02 22:51:23 -0700

    Add husky git hook to build bundle for write-csv

--- write-csv/dist/index.js
+++ write-csv/dist/index.js
@@ -9533,14 +9533,12 @@ const core = __nccwpck_require__(2421)
 const github = __nccwpck_require__(6177)
 const createCsvWriter = (__nccwpck_require__(3863)/* .createObjectCsvWriter */ .eD)
 
-async function run(inputFile, outputCsv) {
-  // console.log('Input file:', inputFile)
+async function run(inputFile, outputSpListCsv) {
   const data = fs.readFileSync(inputFile, 'utf8')
   const parsed = JSON.parse(data)
-  // console.log(JSON.stringify(parsed, null, 2))
-  //const fields = Object.keys(parsed[0].ResponseFields)
-  // console.log('Fields', fields)
-  const fields = [
+
+  /*
+  const inputFields = [
     "responseId",
     "timestamp",
     "0_name",
@@ -9552,21 +9550,50 @@ async function run(inputFile, outputCsv) {
     "2_country_code",
     "3_anything_else_you_want_us_to"
   ]
-  const csvWriter = createCsvWriter({
-    path: outputCsv,
-    header: fields.map(field => ({ id: field, title: field }))
+  */
+
+  // Desired schema:
+  // sp_id,sp_organization,sp_org_id,loc_city,loc_country,loc_continent,active,slack_id
+ 
+  // Examples:
+  // f01392893,"NGVP Cloud",1,"Utrecht","NL","EU",true,"rob"
+  // f01240,"DCENT",2,"Amsterdam","NL","EU",true,"Wijnand Schouten"
+
+  const spListOutputFields = [
+    'sp_id',
+    'sp_organization',
+    'sp_org_id',
+    'loc_city',
+    'loc_country',
+    'loc_continent',
+    'active',
+    'slack_id'
+  ]
+
+  const spListCsvWriter = createCsvWriter({
+    path: outputSpListCsv,
+    header: spListOutputFields.map(field => ({ id: field, title: field }))
   })
-  const records = parsed.map(({ ResponseFields: responseFields }) => ({
-    ...responseFields
+  const spListRecords = parsed.map(({ ResponseFields: responseFields }) => ({
+    sp_id: 1, // FIXME
+    sp_organization: responseFields['0_name'],
+    sp_org_id: 1, // FIXME
+    loc_city: responseFields['1_city'],
+    loc_county: responseFields['1_country_code'],
+    loc_continent: 'XX', // FIXME
+    active: true,
+    slack_id: '@FIXME' // FIXME
   }))
-  await csvWriter.writeRecords(records)
+  await spListCsvWriter.writeRecords(spListRecords)
   console.log('Done.')
 }
 
 try {
   const testResults = core.getInput('test-results') || process.argv[2]
-  const outputCsv = core.getInput('output-csv') || process.argv[3]
-  run(testResults, outputCsv)
+  const outputSpListCsv = core.getInput('output-sp-list-csv') || process.argv[3]
+  const outputOrgsCsv = core.getInput('output-orgs-csv') || process.argv[4]
+  const outputProcessedCsv = core.getInput('processed-csv') || process.argv[5]
+  run(testResults, outputSpListCsv)
 } catch (error) {
   core.setFailed(error.message)
 }
